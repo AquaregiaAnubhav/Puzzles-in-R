5bdb83afa7af8898b7846119ac53848360efa26c
unknown <anubhavprakash420786@gmail.com>
2025-07-12 21:47:50 +0530

    Reorganising files and writing the script for generating game plays

---
+++ Marbles_game_make_a_move/R/episode_gen.R
@@ -0,0 +1,28 @@
+source("game_env.R")
+
+gen_episode <- function(env) {
+  # this will run a ful random gameplay
+  transitions <- list() # we will collect game states as rows here
+  state<- env$reset() # initial state of game
+  
+  repeat{
+    legal <-which(env$valid_moves()) #get indices for valid moves
+    if(length(legal)==0) break #finish if no valid move left
+    action <- sample(legal, 1) # taking a random step to execute
+    result <- env$step(action) #executing the move
+    
+    #storing the transitions
+    transitions[[length(transitions)+1]] <- data.frame(
+      State = state,
+      Action = action,
+      Reward = result$reward,
+      NextState = result$state,
+      stringsAsFactors = FALSE
+    )
+    
+    state <- result$state #moving to the next state
+    
+    if (result$done) break #exiting the game if DONE
+  }
+  do.call(rbind, transitions)
+}
